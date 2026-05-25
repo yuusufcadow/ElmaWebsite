692c734c9e631089680b70ef634e651c18122524
yuusufcadow <yuusufcadow938@gmail.com>
2026-05-25 16:09:05 +0300

    added for scroll smooth with pathname

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+  <ScrollToTop />
     <App />
   </BrowserRouter>
   
